6e2b9c817afb0fb7915d196d28b0bc661e7293b1
Anatoliy <gekonchik9@gmail.com>
2025-01-09 16:03:01 +0200

    Додав оновлення слайдера динамічно
    ваваіавіаіаів

--- Platform/Assets/Scripts/VolumeSettings.cs
+++ Platform/Assets/Scripts/VolumeSettings.cs
@@ -1,14 +1,34 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    [SerializeField] private AudioMixer AudioMixer;
-    [SerializeField] private Slider MusicVolumeSlider;
+    [SerializeField] private AudioMixer audioMixer; // Назви змінних з малої літери
+    [SerializeField] private Slider musicVolumeSlider;
+
+    private const string MusicVolumeKey = "MusicVolume"; // Ключ для PlayerPrefs
+
+    private void Start()
+    {
+        // Завантажуємо збережений рівень гучності або встановлюємо значення за замовчуванням (0.75f)
+        float savedVolume = PlayerPrefs.GetFloat(MusicVolumeKey, 0.75f);
+        musicVolumeSlider.value = savedVolume; // Встановлюємо значення слайдера
+        SetMusicVolume(savedVolume); // Оновлюємо гучність у мікшері
+    }
+
     public void SetMusicVolume()
     {
-        float volume = MusicVolumeSlider.value;
-        AudioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        float volume = musicVolumeSlider.value;
+        SetMusicVolume(volume);
+    }
+
+    private void SetMusicVolume(float volume)
+    {
+        // Застосовуємо рівень гучності до AudioMixer
+        audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+
+        // Зберігаємо рівень гучності в PlayerPrefs
+        PlayerPrefs.SetFloat(MusicVolumeKey, volume);
     }
 }
